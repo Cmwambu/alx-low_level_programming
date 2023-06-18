2acd5e93ffb2657b0e7c75ac5b93e11a7fd5e3a4
Cmwambu <makhanumwambu04@gmail.com>
2023-06-18 08:53:20 -0700

    a

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,20 @@
+/*
+ * File: 0-positive_or_negative.c
+ * Author: [Cmwambu]
+ * Description: A program to print whether a randomly generated number is positive, negative, or zero.
+ * Compilation: gcc -Wall -Werror -Wextra -pedantic -std=gnu89 0-positive_or_negative.c -o program
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function generates a random number and prints whether it is positive, negative, or zero.
+ * Return: 0 on success
+ */
 int main(void)
 {
     int n;
