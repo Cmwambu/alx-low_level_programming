a65211b808b7eb76c6aa826d6b1ad77169f2a5bb
Cmwambu <makhanumwambu04@gmail.com>
2023-06-18 09:39:15 -0700

    a

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+/*
+ * File: 3-print_alphabets.c
+ * Author: [Cmwambu]
+ * Description: A program to print the alphabet in lowercase and uppercase using the putchar function.
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char lowercase;
+	char uppercase;
+
+	for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
+		putchar(lowercase);
+
+	for (uppercase = 'A'; uppercase <= 'Z'; uppercase++)
+		putchar(uppercase);
+
+	putchar('\n');
+
+	return (0);
+}
